7dcb4a3feb4c6aab477f8103814971b08e9f7020
Bryant W <bryantw@ksu.edu>
2017-04-13 14:23:14 -0500

    In-Class Work
    Web sockets and stuff. Some error related to client-side chat.

--- server.js
+++ server.js
@@ -1,34 +1,56 @@
-const PORT = 3000;
+const PORT = 3027;
 
 var fs = require('fs');
 var http = require('http');
 var server = new http.Server(handleRequest);
+var io = require('socket.io')(server);
+
+var connected = 0;
+
+io.on('connection', function(socket) {
+  var color = 'gray';
+  var name = 'User ' + connected;
+  connected++;
+
+  socket.on('message', function(text) {
+    io.emit('message', {
+      user: name,
+      text: text,
+      color: color
+    });
+  });
+
+  socket.on('color', function(newColor) {
+    color = newColor;
+  });
+
+  socket.emit('welcome', "Welcome " + name + "!");
+});
+
+
 
 function handleRequest(req, res) {
   switch(req.url) {
     case '/':
-    case 'index.html':
+    case '/index.html':
       fs.readFile('public/index.html', function(err, data){
-        if(err){
-          res.setHeader("Content-Type", "text/html");
-          res.end(data);
-        }
+        if(err){}
+        res.setHeader("Content-Type", "text/html");
+        res.end(data);
       });
       break;
-    case 'simple-chat.css':
+    case '/simple-chat.css':
       fs.readFile('public/simple-chat', function(err, data){
-        if(err){
-          res.setHeader("Content-Type", "text/css");
-          res.end(data);
-        }
+        if(err){}
+        res.setHeader("Content-Type", "text/css");
+        res.end(data);
       });
       break;
-    case 'simple-chat.js':
+    case '/simple-chat.js':
       fs.readFile('public/simple-chat.js', function(err, data){
-        if(err){
-          res.setHeader("Content-Type", "text/js");
-          res.end(data);
-        }
+        if(err){}
+        res.setHeader("Content-Type", "text/js");
+        res.end(data);
       });
       break;
   }
